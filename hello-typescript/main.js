be472571e565200970b696b29bc2d5f312d2a9ef
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-02-15 08:44:49 -0300

    introduction to typescript

---
+++ hello-typescript/main.js
@@ -0,0 +1,23 @@
+var myVar = 'my variable';
+function myFunction(x, y) {
+    return x + y;
+}
+var num = 2;
+var PI = 3.14;
+var numbers = [1, 2, 3];
+numbers.map(function (valor) {
+    return valor * 2;
+});
+numbers.map(function (valor) {
+    valor * 2;
+});
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.sum = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'sdsasdas';
+n1 = 4;
---
+++ hello-typescript/main.ts
@@ -0,0 +1,26 @@
+var myVar = 'my variable';
+
+function myFunction(x, y) {
+    return x + y;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numbers = [1, 2, 3];
+numbers.map(function (valor) {
+    return valor * 2;
+});
+
+numbers.map(valor => {
+    valor * 2
+});
+
+class Matematica {
+    sum(x, y) {
+        return x + y;
+    }
+}
+
+var n1: any = 'sdsasdas';
+n1 = 4;
