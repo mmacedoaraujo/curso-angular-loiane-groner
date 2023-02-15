78126e9118046a11eaf3c9cada0f2f457b6b94cb
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-02-15 08:26:46 -0300

    generated components

--- first-project/src/app/app.module.ts
+++ first-project/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyFirstComponent } from './my-first/my-first.component';
+import { MySecondComponent } from './my-second/my-second.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponent,
+    MySecondComponent
   ],
   imports: [
     BrowserModule
