5cddac2aab137b7e7c83fa51ed42e8bff92b7c30
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-02-23 21:30:36 -0300

    finalizado: property binding e interpolação

--- databinding/src/app/databinding/databinding.component.ts
+++ databinding/src/app/databinding/databinding.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-databinding',
   templateUrl: './databinding.component.html',
   styleUrls: ['./databinding.component.css']
 })
-export class DatabindingComponent {
+export class DatabindingComponent implements OnInit {
+
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem = 'https://picsum.photos/536/354';
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
